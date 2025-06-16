9ee6c3e9c4318a7a1d57558b4e2ad9e6cad19a55
Anagh Sheth <anagh.sheth@gmail.com>
2025-06-16 16:33:48 -0400

    commit unsaved file

--- src/localblast/urls.py
+++ src/localblast/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for locallift project.
+URL configuration for localblast project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
