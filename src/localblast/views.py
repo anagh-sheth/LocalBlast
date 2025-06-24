c0de71e9d2f1fa8066e489f4008677fc0b03b306
Anagh Sheth <anagh.sheth@gmail.com>
2025-06-24 13:19:21 -0400

    fixed navbar and added about page

--- src/localblast/views.py
+++ src/localblast/views.py
@@ -5,10 +5,23 @@ from django.shortcuts import render
 from visits.models import PageVisit
 
 this_dir = pathlib.Path(__file__).resolve().parent
+
 def home_view(request, *args, **kwargs):
-    if request.user.is_authenticated:
-        print(request.user)
-    return about_view(request, *args, **kwargs)
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    try:
+        percent = (page_qs.count() * 100.0) / qs.count()
+    except:
+        percent = 0
+    my_title = "Home Page"
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count": page_qs.count(),
+        "percent": percent,
+        "total_visits": qs.count(),
+    }
+    PageVisit.objects.create(path=request.path)
+    return render(request, "home.html", my_context)
 
 
 def about_view(request, *args, **kwargs):
@@ -18,13 +31,12 @@ def about_view(request, *args, **kwargs):
         percent = (page_qs.count() * 100.0) / qs.count()
     except:
         percent = 0
-    my_title = "My Page"
-    html_template = "home.html"
+    my_title = "About Page"
     my_context = {
         "page_title": my_title,
         "page_visit_count": page_qs.count(),
         "percent": percent,
-        "total_visit_count": qs.count(),
+        "total_visits": qs.count(),
     }
     PageVisit.objects.create(path=request.path)
-    return render(request, html_template, my_context)
+    return render(request, "about.html", my_context)
