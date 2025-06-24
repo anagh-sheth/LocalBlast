b1fc5618e19541b66507b70c75dc675fffa56b6a
Anagh Sheth <anagh.sheth@gmail.com>
2025-06-24 17:28:42 -0400

    navbar updates

--- src/localblast/urls.py
+++ src/localblast/urls.py
@@ -22,7 +22,7 @@ from localblast.views import home_view, about_view
 
 
 urlpatterns = [
-    path('', home_view),
+    path('', home_view, name='home'),
     path('login/', auth_views.login_view),
     path('register/', auth_views.register_view),
     path('about/', about_view),
