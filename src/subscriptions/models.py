1c1626e2681bc39f9cb3d6c86b30ffed0d7e75b0
Anagh Sheth <anagh.sheth@gmail.com>
2025-07-03 02:29:09 -0400

    added to subscriptions model

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import Group, Permission
+
+SUBSCRIPTION_PERMISSIONS = [
+    ("basic", "Basic"),
+    ("pro", "Pro"),
+]
 
 # Create your models here.
 class Subscription(models.Model):
     name = models.CharField(max_length=120)
+    groups = models.ManyToManyField(Group)
+    permissions = models.ManyToManyField(Permission, 
+                                         limit_choices_to=
+                                         {"content_type__app_label": "subscriptions", "codename__in": [perm[0] for perm in SUBSCRIPTION_PERMISSIONS]})
 
     class Meta:
-        permissions = [
-            ("basic", "Basic"),
-            ("pro", "Pro"),
-        ]
+        permissions = SUBSCRIPTION_PERMISSIONS
