bd8956c9b467d867357c4936ee68305b2a278133
Anagh Sheth <anagh.sheth@gmail.com>
2025-07-10 02:44:11 -0400

    fixing subscription order and setup

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -24,11 +24,17 @@ class Subscription(models.Model):
                                          {"content_type__app_label": "subscriptions", "codename__in": [perm[0] for perm in SUBSCRIPTION_PERMISSIONS]})
     stripe_id = models.CharField(max_length=120, null=True, blank=True)
 
+    order = models.IntegerField(default=-1, help_text="Order on django pricing page")
+    featured = models.BooleanField(default=True, help_text="Featured on django pricing page")
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return f"{self.name}"
 
 
     class Meta:
+        ordering = ["order", "featured", "-updated"]
         permissions = SUBSCRIPTION_PERMISSIONS
 
     def save(self, *args, **kwargs):
@@ -40,6 +46,7 @@ class Subscription(models.Model):
         super().save(*args, **kwargs)
 
 
+
 class SubscriptionPrice(models.Model):
     class IntervalChoices(models.TextChoices):
         MONTHLY  = "month", "Monthly"
@@ -49,8 +56,13 @@ class SubscriptionPrice(models.Model):
     stripe_id = models.CharField(max_length=120, null=True, blank=True)
     interval = models.CharField(max_length=120, default= IntervalChoices.MONTHLY, choices = IntervalChoices.choices)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=99.99)
-    order = models.IntegerField(default=-1)
-    featured = models.BooleanField(default=True)
+    order = models.IntegerField(default=-1, help_text="Order on django pricing page")
+    featured = models.BooleanField(default=True, help_text="Featured on django pricing page")
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["subscription__order", "order", "featured", "-updated"]
 
     @property
     def stripe_currency(self):
@@ -79,6 +91,12 @@ class SubscriptionPrice(models.Model):
                                 raw = False)
             self.stripe_id = stripe_id
         super().save(*args, **kwargs)   
+
+        if self.featured and self.subscription:
+            qs = SubscriptionPrice.objects.filter(subscription = self.subscription, 
+                                                  interval = self.interval,
+                                                  ).exclude(id = self.id)
+            qs.update(featured=False)
 
 
 class UserSubscription(models.Model):
